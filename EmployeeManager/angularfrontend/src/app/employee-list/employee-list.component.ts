6a3c66c265d2b3795eae2209c141f6ba2aeaa151
FabienneFly <fehrfab2@students.zhaw.ch>
2024-03-26 15:09:20 +0100

    Add CRUD functionality and Create Angular Frontend

--- EmployeeManager/angularfrontend/src/app/employee-list/employee-list.component.ts
+++ EmployeeManager/angularfrontend/src/app/employee-list/employee-list.component.ts
@@ -16,11 +16,26 @@ export class EmployeeListComponent implements OnInit {
   employees!: Employee[];
 
   constructor(private employeeService: EmployeeService) {
+
   }
 
   ngOnInit() {
     this.employeeService.getEmployees().subscribe(data => {
       this.employees = data;
+    });
+  }
+
+  deleteEmployee(employeeId: number) {
+    this.employeeService.deleteEmployee(employeeId).subscribe({
+      next: (data) => {
+        // Handle successful delete
+        // For example, refresh the list or show a success message
+        this.employees = this.employees.filter(employee => employee.id !== employeeId);
+      },
+      error: (error) => {
+        // Handle error
+        console.error('There was an error!', error);
+      }
     });
   }
 
