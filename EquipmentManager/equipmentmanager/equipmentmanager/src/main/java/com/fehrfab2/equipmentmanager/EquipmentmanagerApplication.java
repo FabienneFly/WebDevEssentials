bec876a203764fedceff941a236e9fdad65196c6
FabienneFly <fehrfab2@students.zhaw.ch>
2024-03-26 12:49:14 +0100

    Add

---
+++ EquipmentManager/equipmentmanager/equipmentmanager/src/main/java/com/fehrfab2/equipmentmanager/EquipmentmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.fehrfab2.equipmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipmentmanagerApplication.class, args);
+	}
+
+}
---
+++ EquipmentManager/equipmentmanager/equipmentmanager/src/test/java/com/fehrfab2/equipmentmanager/EquipmentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fehrfab2.equipmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
